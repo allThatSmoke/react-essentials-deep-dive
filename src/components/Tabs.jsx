e3f227b45f9966ac2e0a2913291213a924cb2ff5
Govind Dandekar <govind@Govinds-Mini.lan>
2024-11-03 10:36:16 -0800

    Final commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+function Tabs({children, buttons, ButtonContainer="menu"}){
+
+	return (
+		<>
+			<ButtonContainer>
+				{buttons}
+			</ButtonContainer>
+			{children}
+		</>
+	)
+};
+
+export default Tabs;
